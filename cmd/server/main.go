25daead345b4d974ed72482b262d1e61f5926283
dtorannpu <dtorannpu@gmail.com>
2025-12-07 12:26:57 +0900

    リクエストロガーミドルウェア追加; ゼロログ対応追加

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
 )
 
 func main() {
+	logger := zerolog.New(os.Stdout)
+
 	e := echo.New()
+
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:    true,
+		LogStatus: true,
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			logger.Info().
+				Str("URI", v.URI).
+				Int("status", v.Status).
+				Msg("request")
+
+			return nil
+		},
+	}))
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module echo-sample2
 
 go 1.25
 
-require github.com/labstack/echo/v4 v4.13.4
+require (
+	github.com/labstack/echo/v4 v4.13.4
+	github.com/rs/zerolog v1.34.0
+)
 
 require (
 	github.com/labstack/gommon v0.4.2 // indirect
@@ -14,4 +17,5 @@ require (
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
 )

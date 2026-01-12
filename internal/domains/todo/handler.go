e51ea9b7333b3fc28e05222faff500b854a05080
dtorannpu <dtorannpu@gmail.com>
2026-01-12 10:24:37 +0900

    TodoリソースのCRUD操作を実装; ルーティングとテストを追加

--- internal/domains/todo/handler.go
+++ internal/domains/todo/handler.go
@@ -10,18 +10,23 @@ type Handler struct{}
 
 func (h *Handler) RegisterTodoRoutes(e *echo.Echo) {
 	g := e.Group("/todos")
+	g.POST("", h.createTodo)
 	g.GET("", h.getTodos)
 	g.GET(":id", h.getTodo)
-}
-
-func (h *Handler) getTodos(c echo.Context) error {
-	return c.String(http.StatusOK, "Get todos")
-}
-
-func (h *Handler) getTodo(c echo.Context) error {
-	return c.String(http.StatusOK, "Get todo")
+	g.PUT(":id", h.updateTodo)
+	g.DELETE(":id", h.deleteTodo)
 }
 
 func NewTodoHandler() *Handler {
 	return &Handler{}
 }
+
+func (h *Handler) createTodo(c echo.Context) error { return c.NoContent(http.StatusCreated) }
+
+func (h *Handler) getTodos(c echo.Context) error { return c.String(http.StatusOK, "Get todos") }
+
+func (h *Handler) getTodo(c echo.Context) error { return c.String(http.StatusOK, "Get todo") }
+
+func (h *Handler) updateTodo(c echo.Context) error { return c.NoContent(http.StatusNoContent) }
+
+func (h *Handler) deleteTodo(c echo.Context) error { return c.NoContent(http.StatusNoContent) }
--- internal/domains/todo/handler_test.go
+++ internal/domains/todo/handler_test.go
@@ -3,40 +3,69 @@ package todo
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/require"
 )
 
+func TestCreateTodo(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/todos", strings.NewReader(""))
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := NewTodoHandler()
+
+	require.NoError(t, h.createTodo(c))
+	require.Equal(t, http.StatusCreated, rec.Code)
+	require.Equal(t, "", rec.Body.String())
+}
+
 func TestGetTodos(t *testing.T) {
 	e := echo.New()
-
 	req := httptest.NewRequest(http.MethodGet, "/todos", nil)
-	w := httptest.NewRecorder()
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := NewTodoHandler()
 
-	c := e.NewContext(req, w)
-
-	handler := NewTodoHandler()
-	err := handler.getTodos(c)
-
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, w.Code)
-	require.Equal(t, "Get todos", w.Body.String())
+	require.NoError(t, h.getTodos(c))
+	require.Equal(t, http.StatusOK, rec.Code)
+	require.Equal(t, "Get todos", rec.Body.String())
 }
 
 func TestGetTodo(t *testing.T) {
 	e := echo.New()
-
 	req := httptest.NewRequest(http.MethodGet, "/todos/1", nil)
-	w := httptest.NewRecorder()
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := NewTodoHandler()
 
-	c := e.NewContext(req, w)
+	require.NoError(t, h.getTodo(c))
+	require.Equal(t, http.StatusOK, rec.Code)
+	require.Equal(t, "Get todo", rec.Body.String())
+}
 
-	handler := NewTodoHandler()
-	err := handler.getTodo(c)
+func TestUpdateTodo(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPut, "/todos/1", strings.NewReader(""))
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := NewTodoHandler()
 
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, w.Code)
-	require.Equal(t, "Get todo", w.Body.String())
+	require.NoError(t, h.updateTodo(c))
+	require.Equal(t, http.StatusNoContent, rec.Code)
+	require.Equal(t, "", rec.Body.String())
+}
+
+func TestDeleteTodo(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodDelete, "/todos/1", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := NewTodoHandler()
+
+	require.NoError(t, h.updateTodo(c))
+	require.Equal(t, http.StatusNoContent, rec.Code)
+	require.Equal(t, "", rec.Body.String())
 }

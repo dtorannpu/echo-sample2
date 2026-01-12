4d2ce05d30b1df2dcdffb0efe5ae918f59c0c381
dtorannpu <dtorannpu@gmail.com>
2026-01-12 12:05:43 +0900

    `Todoハンドラをリファクタリング; 型をリネームし、ルート定義とテストを整理`

--- internal/domains/todo/handler/handler.go
+++ internal/domains/todo/handler/handler.go
@@ -6,27 +6,63 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type Handler struct{}
+type TodoHandler struct{}
 
-func (h *Handler) RegisterTodoRoutes(e *echo.Echo) {
+type getTodoParams struct {
+	ID int64 `param:"id"`
+}
+
+type updateTodoParams struct {
+	ID int64 `param:"id"`
+}
+
+type deleteTodoParams struct {
+	ID int64 `param:"id"`
+}
+
+func NewTodoHandler() *TodoHandler {
+	return &TodoHandler{}
+}
+
+func (h *TodoHandler) RegisterTodoRoutes(e *echo.Echo) {
 	g := e.Group("/todos")
 	g.POST("", h.createTodo)
 	g.GET("", h.getTodos)
-	g.GET(":id", h.getTodo)
-	g.PUT(":id", h.updateTodo)
-	g.DELETE(":id", h.deleteTodo)
+	g.GET("/:id", h.getTodo)
+	g.PUT("/:id", h.updateTodo)
+	g.DELETE("/:id", h.deleteTodo)
 }
 
-func NewTodoHandler() *Handler {
-	return &Handler{}
+func (h *TodoHandler) createTodo(c echo.Context) error { return c.NoContent(http.StatusCreated) }
+
+func (h *TodoHandler) getTodos(c echo.Context) error { return c.String(http.StatusOK, "Get todos") }
+
+func (h *TodoHandler) getTodo(c echo.Context) error {
+	params := new(getTodoParams)
+
+	if err := c.Bind(params); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.String(http.StatusOK, "Get todo")
 }
 
-func (h *Handler) createTodo(c echo.Context) error { return c.NoContent(http.StatusCreated) }
+func (h *TodoHandler) updateTodo(c echo.Context) error {
+	params := new(updateTodoParams)
 
-func (h *Handler) getTodos(c echo.Context) error { return c.String(http.StatusOK, "Get todos") }
+	if err := c.Bind(params); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
 
-func (h *Handler) getTodo(c echo.Context) error { return c.String(http.StatusOK, "Get todo") }
+	return c.NoContent(http.StatusNoContent)
+}
 
-func (h *Handler) updateTodo(c echo.Context) error { return c.NoContent(http.StatusNoContent) }
+func (h *TodoHandler) deleteTodo(c echo.Context) error {
+	params := new(deleteTodoParams)
 
-func (h *Handler) deleteTodo(c echo.Context) error { return c.NoContent(http.StatusNoContent) }
+	if err := c.Bind(params); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
--- internal/domains/todo/handler/handler_test.go
+++ internal/domains/todo/handler/handler_test.go
@@ -10,62 +10,106 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateTodo(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodPost, "/todos", strings.NewReader(""))
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := NewTodoHandler()
+func TestTodoHandler(t *testing.T) {
+	setup := func() (*echo.Echo, *TodoHandler) {
+		e := echo.New()
+		h := NewTodoHandler()
+		h.RegisterTodoRoutes(e)
+		return e, h
+	}
 
-	require.NoError(t, h.createTodo(c))
-	require.Equal(t, http.StatusCreated, rec.Code)
-	require.Equal(t, "", rec.Body.String())
-}
+	t.Run("CreateTodo", func(t *testing.T) {
+		t.Run("正常系", func(t *testing.T) {
+			e, _ := setup()
+			req := httptest.NewRequest(http.MethodPost, "/todos", strings.NewReader(`{}`))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			e.ServeHTTP(rec, req)
 
-func TestGetTodos(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/todos", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := NewTodoHandler()
+			require.Equal(t, http.StatusCreated, rec.Code)
+			require.Equal(t, "", rec.Body.String())
+		})
 
-	require.NoError(t, h.getTodos(c))
-	require.Equal(t, http.StatusOK, rec.Code)
-	require.Equal(t, "Get todos", rec.Body.String())
-}
+		t.Run("IDが数字じゃない場合", func(t *testing.T) {
+			e, _ := setup()
+			req := httptest.NewRequest(http.MethodGet, "/todos", nil)
+			rec := httptest.NewRecorder()
+			e.ServeHTTP(rec, req)
 
-func TestGetTodo(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodGet, "/todos/1", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := NewTodoHandler()
+			require.Equal(t, http.StatusOK, rec.Code)
+			require.Equal(t, "Get todos", rec.Body.String())
+		})
+	})
 
-	require.NoError(t, h.getTodo(c))
-	require.Equal(t, http.StatusOK, rec.Code)
-	require.Equal(t, "Get todo", rec.Body.String())
-}
+	t.Run("GetTodo", func(t *testing.T) {
+		t.Run("正常系", func(t *testing.T) {
+			e, _ := setup()
+			req := httptest.NewRequest(http.MethodGet, "/todos/1", nil)
+			rec := httptest.NewRecorder()
+			e.ServeHTTP(rec, req)
 
-func TestUpdateTodo(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodPut, "/todos/1", strings.NewReader(""))
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := NewTodoHandler()
+			require.Equal(t, http.StatusOK, rec.Code)
+			require.Equal(t, "Get todo", rec.Body.String())
+		})
 
-	require.NoError(t, h.updateTodo(c))
-	require.Equal(t, http.StatusNoContent, rec.Code)
-	require.Equal(t, "", rec.Body.String())
-}
+		t.Run("IDが数字じゃない場合", func(t *testing.T) {
+			e, _ := setup()
+			req := httptest.NewRequest(http.MethodGet, "/todos/a", nil)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetParamNames("id")
+			c.SetParamValues("a")
+			e.ServeHTTP(rec, req)
 
-func TestDeleteTodo(t *testing.T) {
-	e := echo.New()
-	req := httptest.NewRequest(http.MethodDelete, "/todos/1", nil)
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	h := NewTodoHandler()
+			require.Equal(t, http.StatusBadRequest, rec.Code)
+		})
+	})
 
-	require.NoError(t, h.updateTodo(c))
-	require.Equal(t, http.StatusNoContent, rec.Code)
-	require.Equal(t, "", rec.Body.String())
+	t.Run("UpdateTodo", func(t *testing.T) {
+		t.Run("正常系", func(t *testing.T) {
+			e, _ := setup()
+			req := httptest.NewRequest(http.MethodPut, "/todos/1", strings.NewReader(""))
+			rec := httptest.NewRecorder()
+			e.ServeHTTP(rec, req)
+
+			require.Equal(t, http.StatusNoContent, rec.Code)
+			require.Equal(t, "", rec.Body.String())
+		})
+
+		t.Run("IDが数字じゃない場合", func(t *testing.T) {
+			e, _ := setup()
+			req := httptest.NewRequest(http.MethodPut, "/todos/a", strings.NewReader(`{}`))
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetParamNames("id")
+			c.SetParamValues("a")
+			e.ServeHTTP(rec, req)
+
+			require.Equal(t, http.StatusBadRequest, rec.Code)
+		})
+	})
+
+	t.Run("GetTodos", func(t *testing.T) {
+		t.Run("正常系", func(t *testing.T) {
+			e, _ := setup()
+			req := httptest.NewRequest(http.MethodDelete, "/todos/1", nil)
+			rec := httptest.NewRecorder()
+			e.ServeHTTP(rec, req)
+
+			require.Equal(t, http.StatusNoContent, rec.Code)
+			require.Equal(t, "", rec.Body.String())
+		})
+
+		t.Run("IDが数字じゃない場合", func(t *testing.T) {
+			e, _ := setup()
+			req := httptest.NewRequest(http.MethodDelete, "/todos/a", strings.NewReader(`{}`))
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetParamNames("id")
+			c.SetParamValues("a")
+			e.ServeHTTP(rec, req)
+
+			require.Equal(t, http.StatusBadRequest, rec.Code)
+		})
+	})
 }
